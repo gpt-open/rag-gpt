69832bd76fca9b98c08ed5939189b04f9560401d
blmdxiao <blmdxiao@gmail.com>
2024-06-18 18:45:31 +0800

    Update model list of ZhipuAI

--- server/constant/env_constants.py
+++ server/constant/env_constants.py
@@ -2,7 +2,6 @@ import os
 import sys
 from server.logger.logger_config import my_logger as logger
 
-
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
@@ -11,7 +10,8 @@ def check_env_variables():
     LLM_NAME = os.getenv('LLM_NAME')
     llm_name_list = ['OpenAI', 'ZhipuAI', 'Ollama', 'DeepSeek']
     if LLM_NAME not in llm_name_list:
-        logger.error(f"LLM_NAME: '{LLM_NAME}' is illegal! Must be in {llm_name_list}.")
+        logger.error(
+            f"LLM_NAME: '{LLM_NAME}' is illegal! Must be in {llm_name_list}.")
         sys.exit(-1)
 
     if LLM_NAME == 'OpenAI':
@@ -25,7 +25,9 @@ def check_env_variables():
         GPT_MODEL_NAME = os.getenv('GPT_MODEL_NAME')
         gpt_model_name_list = ['gpt-3.5-turbo', 'gpt-4-turbo', 'gpt-4o']
         if GPT_MODEL_NAME not in gpt_model_name_list:
-            logger.error(f"GPT_MODEL_NAME: '{GPT_MODEL_NAME}' is illegal! Must be in {gpt_model_name_list}")
+            logger.error(
+                f"GPT_MODEL_NAME: '{GPT_MODEL_NAME}' is illegal! Must be in {gpt_model_name_list}"
+            )
             sys.exit(-1)
     elif LLM_NAME == 'ZhipuAI':
         # ZHIPUAI_API_KEY: API key for accessing ZhipuAI's services.
@@ -36,25 +38,37 @@ def check_env_variables():
 
         # GLM_MODEL_NAME: Specific GLM model being used, e.g., 'glm-3-turbo' or 'glm-4'.
         GLM_MODEL_NAME = os.getenv('GLM_MODEL_NAME')
-        if GLM_MODEL_NAME not in ['glm-3-turbo', 'glm-4']:
-            logger.error(f"GLM_MODEL_NAME: '{GLM_MODEL_NAME}' is illegal! Must be 'glm-3-turbo' or 'glm-4'")
+        if GLM_MODEL_NAME not in [
+                'glm-3-turbo', 'glm-4', 'glm-4-0520', 'glm-4-air',
+                'glm-4-airx', 'glm-4-flash'
+        ]:
+            logger.error(
+                f"GLM_MODEL_NAME: '{GLM_MODEL_NAME}' is illegal! Must be in ['glm-3-turbo', 'glm-4', 'glm-4-0520', 'glm-4-air', 'glm-4-airx', 'glm-4-flash']"
+            )
             sys.exit(-1)
     elif LLM_NAME == 'Ollama':
         # OLLAMA_MODEL_NAME: Specific Ollma model being used, e.g., 'llama3', 'llama3:70b', 'phi3', 'mistral', etc.
         OLLAMA_MODEL_NAME = os.getenv('OLLAMA_MODEL_NAME')
         if OLLAMA_MODEL_NAME == 'xxxx':
-            logger.error(f"OLLAMA_MODEL_NAME: '{OLLAMA_MODEL_NAME}' is illegal! Mast be 'llama3', 'llama3:70b', 'phi3', 'mistral', etc.")
+            logger.error(
+                f"OLLAMA_MODEL_NAME: '{OLLAMA_MODEL_NAME}' is illegal! Mast be 'llama3', 'llama3:70b', 'phi3', 'mistral', etc."
+            )
             sys.exit(-1)
 
         OLLAMA_BASE_URL = os.getenv('OLLAMA_BASE_URL')
-        if not OLLAMA_BASE_URL.startswith('http://') and not OLLAMA_BASE_URL.startswith('https://'):
-            logger.error(f"OLLAMA_BASE_URL: '{OLLAMA_BASE_URL}' is illegal! It must be like 'http://IP:PORT' or 'https://IP:PORT'")
+        if not OLLAMA_BASE_URL.startswith(
+                'http://') and not OLLAMA_BASE_URL.startswith('https://'):
+            logger.error(
+                f"OLLAMA_BASE_URL: '{OLLAMA_BASE_URL}' is illegal! It must be like 'http://IP:PORT' or 'https://IP:PORT'"
+            )
             sys.exit(-1)
 
         # Count the number of '/'
         slash_count = OLLAMA_BASE_URL.count('/')
         if slash_count > 2:
-            logger.error(f"OLLAMA_BASE_URL: '{OLLAMA_BASE_URL}' is illegal! It must be like 'http://IP:PORT' or 'https://IP:PORT'")
+            logger.error(
+                f"OLLAMA_BASE_URL: '{OLLAMA_BASE_URL}' is illegal! It must be like 'http://IP:PORT' or 'https://IP:PORT'"
+            )
             sys.exit(-1)
     elif LLM_NAME == 'DEEPSEEK':
         # ZHIPUAI_API_KEY: API key for accessing ZhipuAI's services.
@@ -72,7 +86,9 @@ def check_env_variables():
         # DEEPSEEK_MODEL_NAME: Specific DeepSeek model being used, e.g., 'deepseek-chat' or 'deepseek-coder'.
         DEEPSEEK_MODEL_NAME = os.getenv('DEEPSEEK_MODEL_NAME')
         if DEEPSEEK_MODEL_NAME not in ['deepseek-chat', 'deepseek-coder']:
-            logger.error(f"DEEPSEEK_MODEL_NAME: '{DEEPSEEK_MODEL_NAME}' is illegal! Must be 'deepseek-chat' or 'deepseek-coder'")
+            logger.error(
+                f"DEEPSEEK_MODEL_NAME: '{DEEPSEEK_MODEL_NAME}' is illegal! Must be 'deepseek-chat' or 'deepseek-coder'"
+            )
             sys.exit(-1)
 
     # MIN_RELEVANCE_SCORE: Minimum score for a document to be considered relevant, and will be used in prompt, between 0.3 and 0.7.
@@ -80,22 +96,31 @@ def check_env_variables():
     try:
         min_relevance_score = float(MIN_RELEVANCE_SCORE)
         if min_relevance_score < 0.3 or min_relevance_score > 0.7:
-            logger.error(f"MIN_RELEVANCE_SCORE: {MIN_RELEVANCE_SCORE} is illegal! It should be a float number between [0.3~0.7]")
+            logger.error(
+                f"MIN_RELEVANCE_SCORE: {MIN_RELEVANCE_SCORE} is illegal! It should be a float number between [0.3~0.7]"
+            )
             sys.exit(-1)
     except Exception as e:
-        logger.error(f"MIN_RELEVANCE_SCORE: {MIN_RELEVANCE_SCORE} is illegal! It should be a float number between [0.3~0.7]")
+        logger.error(
+            f"MIN_RELEVANCE_SCORE: {MIN_RELEVANCE_SCORE} is illegal! It should be a float number between [0.3~0.7]"
+        )
         sys.exit(-1)
 
     # BOT_TOPIC: Main topic or domain the bot is designed to handle, like 'OpenIM' or 'LangChain'.
     BOT_TOPIC = os.getenv('BOT_TOPIC')
     if BOT_TOPIC == 'xxxx':
-        logger.error(f"BOT_TOPIC: '{BOT_TOPIC}' is illegal! You must set your own bot topic, such as 'OpenIM' or 'LangChain', etc.")
+        logger.error(
+            f"BOT_TOPIC: '{BOT_TOPIC}' is illegal! You must set your own bot topic, such as 'OpenIM' or 'LangChain', etc."
+        )
         sys.exit(-1)
 
     # URL_PREFIX: The prefix URL for accessing media and other resources, must start with 'http://' or 'https://'.
     URL_PREFIX = os.getenv('URL_PREFIX')
-    if not URL_PREFIX.startswith('http://') and not URL_PREFIX.startswith('https://'):
-        logger.error(f"URL_PREFIX: '{URL_PREFIX}' is illegal! It must start with 'http://' or 'https://'")
+    if not URL_PREFIX.startswith('http://') and not URL_PREFIX.startswith(
+            'https://'):
+        logger.error(
+            f"URL_PREFIX: '{URL_PREFIX}' is illegal! It must start with 'http://' or 'https://'"
+        )
         sys.exit(-1)
 
     # USE_PREPROCESS_QUERY: Flag (0 or 1) indicating whether preprocessing should be applied to queries.
@@ -103,10 +128,14 @@ def check_env_variables():
     try:
         use_preprocess_query = int(USE_PREPROCESS_QUERY)
         if use_preprocess_query not in [0, 1]:
-            logger.error(f"USE_PREPROCESS_QUERY: {USE_PREPROCESS_QUERY} is illegal! It should be 0 or 1!")
+            logger.error(
+                f"USE_PREPROCESS_QUERY: {USE_PREPROCESS_QUERY} is illegal! It should be 0 or 1!"
+            )
             sys.exit(-1)
     except Exception as e:
-        logger.error(f"USE_PREPROCESS_QUERY: {USE_PREPROCESS_QUERY} is illegal! It should be 0 or 1!")
+        logger.error(
+            f"USE_PREPROCESS_QUERY: {USE_PREPROCESS_QUERY} is illegal! It should be 0 or 1!"
+        )
         sys.exit(-1)
 
     # USE_RERANKING: Flag (0 or 1) indicating whether reranking should be applied to search results.
@@ -114,10 +143,13 @@ def check_env_variables():
     try:
         use_reranking = int(USE_RERANKING)
         if use_reranking not in [0, 1]:
-            logger.error(f"USE_RERANKING: {USE_RERANKING} is illegal! It should be 0 or 1!")
+            logger.error(
+                f"USE_RERANKING: {USE_RERANKING} is illegal! It should be 0 or 1!"
+            )
             sys.exit(-1)
     except Exception as e:
-        logger.error(f"USE_RERANKING: {USE_RERANKING} is illegal! It should be 0 or 1!")
+        logger.error(
+            f"USE_RERANKING: {USE_RERANKING} is illegal! It should be 0 or 1!")
         sys.exit(-1)
 
     # USE_DEBUG: Flag (0 or 1) indicating whether to output additional debug information, such as `search`, `reranking`, `prompt`.
@@ -125,8 +157,10 @@ def check_env_variables():
     try:
         use_debug = int(USE_DEBUG)
         if use_debug not in [0, 1]:
-            logger.error(f"USE_DEBUG: {USE_DEBUG} is illegal! It should be 0 or 1!")
+            logger.error(
+                f"USE_DEBUG: {USE_DEBUG} is illegal! It should be 0 or 1!")
             sys.exit(-1)
     except Exception as e:
-        logger.error(f"USE_DEBUG: {USE_DEBUG} is illegal! It should be 0 or 1!")
+        logger.error(
+            f"USE_DEBUG: {USE_DEBUG} is illegal! It should be 0 or 1!")
         sys.exit(-1)
